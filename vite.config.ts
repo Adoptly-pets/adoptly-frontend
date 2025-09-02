c9108cf3a44ed6d8f22e518b4a8ed80aaaf34b1f
Dmytro Furtune <d.furtune18@gmail.com>
2025-09-02 19:16:09 +0300

    Update CI configuration to include copying index.html to 404.html and downgrade actions-gh-pages version

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/adoptly-frontend/', // Замініть <repository-name> на назву вашого репозиторію
 });
