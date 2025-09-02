895c455f305d82ab1666099d96e1b7cba4378547
Verona0609 <dikaya.nika15@gmail.com>
2025-09-02 18:23:22 +0200

    some changes

--- src/components/ReadyToAdopt/ReadyToAdopt.tsx
+++ src/components/ReadyToAdopt/ReadyToAdopt.tsx
@@ -1,35 +1,25 @@
 import './ReadyToAdopt.css';
 const ReadyToAdopt = () => {
-  return (
-    <section className="ready-to-adopt">
-      <div className="text-box">
-        <h3 className="title">Готовий зустріти нового друга?</h3>
-        <p className="description">
-          Обирай фільтри - котик чи собачка, вік, розмір та інші параметри і
-          починай пошук друга прямо зараз.
-        </p>
-        <a
-          href="#findpet"
-          className="search-btn"
-          onClick={() => alert('Кнопка "Почати пошук улюбленця" натиснута!')}
-        >
-          Почати пошук улюбленця
-        </a>
-      </div>
+    return (
+        <section className="ready-to-adopt">
+            <div className="text-box">
+                <h3 className="title">Готовий зустріти нового друга?</h3>
+                <p className="description">
+                    Обирай фільтри - котик чи собачка, вік, розмір та інші параметри і
+                    починай пошук друга прямо зараз.
+                </p>
+                <a href="#findpet"
+                    className="search-btn">
+                    Почати пошук улюбленця
+                </a>
 
-      <img className="img-dog" src="images/Dog.svg" alt="dog" />
-      <img className="img-cat" src="images/Cat.svg" alt="cat" />
-    </section>
-  );
+
+            </div>
+
+            <img className="img-dog" src="images/Dog.svg" alt="dog" />
+            <img className="img-cat" src="images/Cat.svg" alt="cat" />
+        </section>
+    );
 };
 export default ReadyToAdopt;
 
-{
-  /*  <button
-          onClick={() => alert('Кнопка "Почати пошук улюбленця“ натиснута!')}
-          type="button"
-          className="search-btn"
-        >
-          Почати пошук улюбленця
-        </button> */
-}
