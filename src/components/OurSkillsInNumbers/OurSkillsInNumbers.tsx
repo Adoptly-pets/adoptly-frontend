1ec4eb70021443dfcfff9d2c0268603ef764ec46
Dmytro Furtune <d.furtune18@gmail.com>
2025-09-13 23:05:13 +0300

    Add mobile version for NumberCard and OurSkillsInNumber comoponents. Fix tests

--- src/components/OurSkillsInNumbers/OurSkillsInNumbers.tsx
+++ src/components/OurSkillsInNumbers/OurSkillsInNumbers.tsx
@@ -1,14 +1,16 @@
 import NumberCard from '../NumberCard/NumberCard';
 import './OurSkillsInNumbers.css';
 import { NUMBER_CARD_DATA } from '../../constants/NUMBER_CARD_DATA';
+import Button from '../Button/Button';
 
 const OurSkillsInNumbers = () => {
   return (
     <section className="our-skills-in-numbers">
       <h2>Наш вплив у цифрах</h2>
-      <div className="text-grid">
+      <div className="our-skills-in-numbers__text-grid">
         <p>
-          Назва - платформа, де кожна людина може знайти свого вірного друга.
+          <strong>Adoptly</strong> - платформа, де кожна людина може знайти
+          свого вірного друга.
         </p>
         <p>
           Тут ти можеш побачити фото, прочитати історії, відчути зв’язок і
@@ -25,15 +27,20 @@ const OurSkillsInNumbers = () => {
           нове життя.
         </p>
       </div>
-      <div className="cards">
-        {NUMBER_CARD_DATA.map((card, index) => (
+      <div className="our-skills-in-numbers__cards">
+        {NUMBER_CARD_DATA.map(card => (
           <NumberCard
-            key={index}
+            key={card.id}
             cardNumber={card.cardNumber}
             cardText={card.cardText}
           />
         ))}
       </div>
+      <Button
+        children="Дізнатись більше"
+        variant="secondary"
+        onClick={() => {}}
+      ></Button>
     </section>
   );
 };
