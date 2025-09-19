946cbb98bb5c9fbfaf9e952eca4f16e005ba7c18
Svitlana-G <svitlana.golubieva@gmail.com>
2025-09-19 15:35:11 +0200

    add localization

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,23 +1,26 @@
 import { Link } from 'react-router-dom';
 import './Navigation.css';
+import { useTranslation } from 'react-i18next';
+
 const Navigation = () => {
+  const { t } = useTranslation();
   return (
     <nav>
       <ul className="list">
         <li className="item">
-          <Link to="/">Головна</Link>
+          <Link to="/">{t('nav.home')}</Link>
         </li>
         <li className="item">
-          <Link to="/about">Про нас</Link>
+          <Link to="/about">{t('nav.about')}</Link>
         </li>
         <li className="item">
-          <Link to="/how-to-help">Як допомогти</Link>
+          <Link to="/how-to-help">{t('nav.help')}</Link>
         </li>
         <li className="item">
-          <Link to="/shelters">Притулкам</Link>
+          <Link to="/shelters">{t('nav.shelters')}</Link>
         </li>
         <li className="item">
-          <Link to="/contacts">Контакти</Link>
+          <Link to="/contacts">{t('nav.contacts')}</Link>
         </li>
       </ul>
     </nav>
