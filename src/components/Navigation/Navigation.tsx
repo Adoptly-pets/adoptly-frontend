7b981362ba83b0065a936b4edf1eb3fe4d50fee2
Dmytro Furtune <d.furtune18@gmail.com>
2025-09-26 19:30:28 +0300

    [OSTC-113] Update button dimensions and refactor ModalNavigation to use Navigation component

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,23 +1,46 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
-const Navigation = () => {
+
+// Navigation component code
+interface NavigationProps {
+  className?: string;
+  onLinkClick?: () => void;
+}
+
+const Navigation: React.FC<NavigationProps> = ({
+  className = '',
+  onLinkClick,
+}) => {
+  const listClass = `list ${className}`.trim();
+
   return (
     <nav>
-      <ul className="list">
+      <ul className={listClass}>
         <li className="item">
-          <Link to="/">Головна</Link>
+          <Link to="/" onClick={onLinkClick}>
+            Головна
+          </Link>
         </li>
         <li className="item">
-          <Link to="/about">Про нас</Link>
+          <Link to="/about" onClick={onLinkClick}>
+            Про нас
+          </Link>
         </li>
         <li className="item">
-          <Link to="/how-to-help">Як допомогти</Link>
+          <Link to="/how-to-help" onClick={onLinkClick}>
+            Як допомогти
+          </Link>
         </li>
         <li className="item">
-          <Link to="/shelters">Притулкам</Link>
+          <Link to="/shelters" onClick={onLinkClick}>
+            Притулкам
+          </Link>
         </li>
         <li className="item">
-          <Link to="/contacts">Контакти</Link>
+          <Link to="/contacts" onClick={onLinkClick}>
+            Контакти
+          </Link>
         </li>
       </ul>
     </nav>
