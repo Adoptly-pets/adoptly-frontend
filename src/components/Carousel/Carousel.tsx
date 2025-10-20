1ec6c26bb1f58d5e148d893cc462c7322d84798a
Svitlana-G <svitlana.golubieva@gmail.com>
2025-10-20 16:56:17 +0200

    add disabled state logic and styles for carousel navigation buttons

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,8 +1,9 @@
-import {useRef} from 'react';
+import {useRef, useState} from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import  {Navigation} from 'swiper/modules';
+import  {Navigation, Pagination} from 'swiper/modules';
 import {Swiper as SwiperType} from 'swiper';
 import 'swiper/css/bundle';
+import 'swiper/css/pagination';
 import './Carousel.css';
 import djeck from '../../assets/images/happy_stories/djeck.webp';
 import Roksi from '../../assets/images/happy_stories/Roksi.webp';
@@ -12,6 +13,8 @@ import { Icon } from '../Icon/Icon';
 
 const Carousel = () => {
   const swiperRef = useRef<SwiperType | null>(null);
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
 
 
   const handlePrevSlide = () => {
@@ -22,26 +25,37 @@ const Carousel = () => {
     swiperRef.current?.slideNext();
   };
 
+  const handleSlideChange = (swiper: SwiperType) => {
+    setIsBeginning(swiper.isBeginning);
+    setIsEnd(swiper.isEnd);
+  };
+
+
   return (
     <div className="carousel-container">
 
       <button
-        className="carousel-nav-btn carousel-nav-btn--prev"
         onClick={handlePrevSlide}
         aria-label="Попередній слайд"
       >
-        <Icon id="icon-left" size={43} height={43}/>
+        <Icon id="icon-left" size={43} height={43} className={isBeginning ? 'carousel-nav-btn--disabled' : ''}/>
       </button>
 
       <Swiper
-        modules={[Navigation]}
+        modules={[Navigation, Pagination]}
         onSwiper={(swiper) => {
           swiperRef.current = swiper;
+          setIsBeginning(swiper.isBeginning);
+          setIsEnd(swiper.isEnd);
         }}
-
+        onSlideChange={handleSlideChange}
         slidesPerView={2}
         slidesPerGroup={2}
         spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+
       >
         <SwiperSlide>
           <img src={djeck} alt="Djeck" />
@@ -58,13 +72,12 @@ const Carousel = () => {
       </Swiper>
 
       <button
-        className="carousel-nav-btn carousel-nav-btn--next"
+        className={isEnd ? 'carousel-nav-btn--disabled' : ''}
         onClick={handleNextSlide}
         aria-label="Наступний слайд"
       >
         <Icon id="icon-right" size={43} height={43}/>
       </button>
-
     </div>
   );
 };
