8ea1ff8ea1c82e3cecfc0cc4a0becf7618fd8ad0
Svitlana-G <svitlana.golubieva@gmail.com>
2025-10-31 16:33:07 +0100

    Add title and button

--- src/components/ShelterSupportSection/ShelterSupportSection.tsx
+++ src/components/ShelterSupportSection/ShelterSupportSection.tsx
@@ -1,6 +1,19 @@
+import Button from '../Button/Button';
 import './ShelterSupportSection.css';
 
 const ShelterSupportSection = () => {
-  return <div className="shelter-suport-section"></div>;
+  return (
+    <section className="shelter-suport-section">
+      <div className="content-container">
+        <h3>
+          Додавайте тварин у каталог та отримуйте донати, корм та ліки від
+          небайдужих людей
+        </h3>
+        <Button variant="primary" maxWidth={396}>
+          Зареєструвати притулок
+        </Button>
+      </div>
+    </section>
+  );
 };
 export default ShelterSupportSection;
