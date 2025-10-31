91f574b2f5aa113f45f93d734c9db60ab89196f0
Verona0609 <156179525+Verona0609@users.noreply.github.com>
2025-10-31 09:51:48 +0100

    change route im Footer.test

--- src/components/FAQAccordionSection/FAQAccordionSection.tsx
+++ src/components/FAQAccordionSection/FAQAccordionSection.tsx
@@ -1,4 +1,4 @@
-import Accordionitem from '../../components/AccordionItem/AccordionItem';
+import AccordionItem from '../../components/AccordionItem/AccordionItem';
 import { FAQ_DATA } from '../../constants/FAQ_DATA';
 import './FAQAccordionSection.css';
 
@@ -7,7 +7,7 @@ const FAQAccordionSection = () => {
     <div className="faq-section">
       <h2>Часті запитання</h2>
       {FAQ_DATA.map(item => (
-        <Accordionitem
+        <AccordionItem
           key={item.id}
           question={item.question}
           answer={item.answer}
