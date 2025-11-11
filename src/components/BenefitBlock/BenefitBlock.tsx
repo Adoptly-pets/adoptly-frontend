22b4fb6693a6c843826c15abba00548de5239601
Svitlana-G <svitlana.golubieva@gmail.com>
2025-11-11 17:41:09 +0100

    Add BenefitBlock component

---
+++ src/components/BenefitBlock/BenefitBlock.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BenefitBlock.css';
+
+interface BenefitBlockProps {
+  image: string;
+  title: string;
+  text: string;
+}
+const BenefitBlock: React.FC<BenefitBlockProps> = ({ image, title, text }) => {
+  return (
+    <div className="benefit-block">
+      <img src={image} alt={title} className="block-image" />
+      <h4>{title}</h4>
+      <p>{text}</p>
+    </div>
+  );
+};
+export default BenefitBlock;
--- src/components/BenefitsOfCooperation/BenetisOfCoopertation.tsx
+++ src/components/BenefitsOfCooperation/BenetisOfCoopertation.tsx
@@ -1,7 +1,17 @@
 import './BenefitsOfCooperation.css';
+import BenefitBlock from '../BenefitBlock/BenefitBlock';
+import GuideCardImg from '../../assets/images/guideCardImg1.webp';
 
 const BenefitsOfCooperation = () => {
-  return <div className="benefits">Benefits</div>;
+  return (
+    <div className="benefits">
+      <BenefitBlock
+        image={GuideCardImg}
+        title="Заявки на усиновлення"
+        text="Отримуйте запити від людей, які хочуть забрати тварину додому"
+      />
+    </div>
+  );
 };
 
 export default BenefitsOfCooperation;
