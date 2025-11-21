95047edbf639ff65c18d909a9b748ccd4ad3a3fb
Dmytro Furtune <d.furtune18@gmail.com>
2025-11-21 21:22:06 +0200

    [OSTC-95] add loading lazy

--- src/components/BenefitBlock/BenefitBlock.tsx
+++ src/components/BenefitBlock/BenefitBlock.tsx
@@ -9,7 +9,7 @@ interface BenefitBlockProps {
 const BenefitBlock: React.FC<BenefitBlockProps> = ({ image, title, text }) => {
   return (
     <div className="benefit-block">
-      <img src={image} alt={title} className="block-image" />
+      <img src={image} alt={title} className="block-image" loading="lazy" />
       <h4>{title}</h4>
       <p>{text}</p>
     </div>
