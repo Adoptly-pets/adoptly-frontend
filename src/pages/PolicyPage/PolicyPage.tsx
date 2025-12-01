f1a23e032a0e9b1b2f4935c413dc4af10181f4e0
Svitlana-G <svitlana.golubieva@gmail.com>
2025-12-01 17:03:36 +0100

    Add translations for the title

--- src/pages/PolicyPage/PolicyPage.tsx
+++ src/pages/PolicyPage/PolicyPage.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next';
 import './PolicyPage.css';
 
 const PolicyPage = () => {
+  const { t } = useTranslation();
   return (
     <div className="container privacy-policy">
-      <h1>Політика конфіденційності</h1>
+      <h1>{t('privacyPolicy.title')}</h1>
     </div>
   );
 };
