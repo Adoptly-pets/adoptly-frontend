0b6d89f2037dd7a189b6b9a5d22327aec7815fc3
Svitlana-G <svitlana.golubieva@gmail.com>
2026-03-31 20:03:35 +0200

    Add Netlify support with dynamic base path

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import ShelterDashboardPage from './pages/ShelterDashboardPage/ShelterDashboardP
 
 function App() {
   return (
-    <Router basename="/adoptly-frontend">
+    <Router basename={import.meta.env.BASE_URL.replace(/\/$/, '')}>
       <Header />
       <main>
         <Routes>
